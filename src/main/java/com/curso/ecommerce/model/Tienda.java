82889956883f455fa046b0bf6586d33b30b72550
V4LERIAN0 <eliiasvaleriano@gmail.com>
2025-04-05 11:43:16 -0600

    Versión antes de cambios finales

--- src/main/java/com/curso/ecommerce/model/Tienda.java
+++ src/main/java/com/curso/ecommerce/model/Tienda.java
@@ -1,105 +1,78 @@
 package com.curso.ecommerce.model;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "tiendas")
 public class Tienda {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+
 	private String nombre;
 	private String descripcion;
 	private String imagen;
-	private double precio;
-	private int cantidad;
 
+	// Removed precio and cantidad fields
+
+	// The owner of the store (the admin who created it)
 	@ManyToOne
-	private Usuario usuario;
+	@JoinColumn(name = "usuario_id", nullable = false)
+	private Usuario owner;
 
+	// The list of products belonging to this store
+	@OneToMany(mappedBy = "tienda", cascade = CascadeType.ALL, orphanRemoval = true)
+	private List<Producto> productos = new ArrayList<>();
 
-	//constructor
-	public Tienda() {}
+	// Constructors
+	public Tienda() { }
 
-	public Tienda(Integer id, String nombre, String descripcion, String imagen, double precio, int cantidad) {
+	public Tienda(Integer id, String nombre, String descripcion, String imagen, Usuario owner) {
 		this.id = id;
 		this.nombre = nombre;
 		this.descripcion = descripcion;
 		this.imagen = imagen;
-		this.precio = precio;
-		this.cantidad = cantidad;
+		this.owner = owner;
 	}
 
-
-	//getter and setter
-
-
+	// Getters and Setters
 	public Integer getId() {
 		return id;
 	}
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
 	public String getNombre() {
 		return nombre;
 	}
-
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
-
 	public String getDescripcion() {
 		return descripcion;
 	}
-
 	public void setDescripcion(String descripcion) {
 		this.descripcion = descripcion;
 	}
-
 	public String getImagen() {
 		return imagen;
 	}
-
 	public void setImagen(String imagen) {
 		this.imagen = imagen;
 	}
-
-	public double getPrecio() {
-		return precio;
+	public Usuario getOwner() {
+		return owner;
 	}
-
-	public void setPrecio(double precio) {
-		this.precio = precio;
+	public void setOwner(Usuario owner) {
+		this.owner = owner;
 	}
-
-	public int getCantidad() {
-		return cantidad;
+	public List<Producto> getProductos() {
+		return productos;
 	}
-
-	public void setCantidad(int cantidad) {
-		this.cantidad = cantidad;
-	}
-
-	public Usuario getUsuario() {
-		return usuario;
-	}
-
-	public void setUsuario(Usuario usuario) {
-		this.usuario = usuario;
-	}
-
-	@Override
-	public String toString() {
-		return "Tienda{" +
-				"id=" + id +
-				", nombre='" + nombre + '\'' +
-				", descripcion='" + descripcion + '\'' +
-				", imagen='" + imagen + '\'' +
-				", precio=" + precio +
-				", cantidad=" + cantidad +
-				", usuario=" + usuario +
-				'}';
+	public void setProductos(List<Producto> productos) {
+		this.productos = productos;
 	}
 }
